58f941b952d6ca746aaad2e5cee7673dc9de3a41
Mr.Smoke <kumroshan120@gmail.com>
2023-03-19 21:33:30 +0530

    booking place and discription Start

--- airbnb-app/src/assets/Components/Pages/PlacePage.jsx
+++ airbnb-app/src/assets/Components/Pages/PlacePage.jsx
@@ -4,7 +4,10 @@ import { useParams } from 'react-router-dom';
 import { Loading } from '../Extras/Loading';
 // import { ErrorPage } from '../Extras/ErrorPage';
 import { TiDeleteOutline, TiLocation, TiStarFullOutline } from 'react-icons/ti';
-import { TbBellRingingFilled, TbGridDots } from 'react-icons/tb';
+import { TbBellRingingFilled } from 'react-icons/tb';
+import { Booking } from '../Extras/Booking';
+import { PlaceImages } from '../Extras/PlaceImages';
+import { AllPhotos } from '../Extras/AllPhotos';
 
 export const PlacePage = () => {
   const { id } = useParams();
@@ -38,35 +41,7 @@ export const PlacePage = () => {
   if (!place) return '';
 
   if (showAllPhotos) {
-    return (
-      <div className="absolute inset-0 bg-black text-white opacity-85 min-h-fit p-8">
-        <div className="">
-          <h2 className="text-3xl ">Phots of {place.title}</h2>
-          <button
-            onClick={() => {
-              setShowAllPhotos(false);
-            }}
-            className="fixed right-8 top-8 text-black flex items-center gap-1 px-2 bg-gray-300 rounded-2xl duration-300 hover:scale-110 hover:bg-rose-500 shadow-md shadow-black-400 cursor-pointer">
-            <TiDeleteOutline className="w-auto h-10" />
-            Close
-          </button>
-        </div>
-        <div className="grid pt-4 gap-3 justify-items-center">
-          {place?.photos?.length > 0 &&
-            place.photos.map((photo) => {
-              return (
-                <div className="w-3/6 h-fit text-center">
-                  <img
-                    src={'http://localhost:4000/uploads/' + photo}
-                    alt={photo}
-                    className="rounded-2xl"
-                  />
-                </div>
-              );
-            })}
-        </div>
-      </div>
-    );
+    return <AllPhotos placeProps={place} AllPhotos={setShowAllPhotos} />;
   }
 
   return (
@@ -77,7 +52,7 @@ export const PlacePage = () => {
         <div className="mt-8 ">
           <h1 className="text-3xl font-medium">{place.title}</h1>
 
-          <div className="my-2 items-center gap-2 font-semibold md:flex">
+          <div className="my-2 items-center gap-2 font-semibold flex">
             <TiStarFullOutline />
             <h3>4.3</h3>
             <span>.</span>
@@ -92,62 +67,29 @@ export const PlacePage = () => {
               {place.address}
             </a>
           </div>
-          <div className="relative">
-            <div className="grid gap-2 grid-cols-[2fr_1fr_1fr] rounded-2xl overflow-hidden">
-              {place.photos?.[0] && (
-                <div className="aspect-square object-cover">
-                  <img
-                    className="aspect-square object-cover"
-                    src={'http://localhost:4000/uploads/' + place.photos[0]}
-                    alt={place.photos[0]}
-                  />
-                </div>
-              )}
-              <div className="grid">
-                {place.photos?.[1] && (
-                  <img
-                    className="aspect-square object-cover"
-                    src={'http://localhost:4000/uploads/' + place.photos[1]}
-                    alt={place.photos[1]}
-                  />
-                )}
-                <div className="overflow-hidden">
-                  {place.photos?.[2] && (
-                    <img
-                      className="aspect-square object-cover relative top-2"
-                      src={'http://localhost:4000/uploads/' + place.photos[2]}
-                      alt={place.photos[2]}
-                    />
-                  )}
-                </div>
+          <PlaceImages placeProps={place} allPhotos={setShowAllPhotos} />
+          <div className="grid grid-cols-1 md:grid-cols-[2fr_1fr] gap-8 my-8">
+            <div className=" bg-gray-300 h-fit p-4 rounded-2xl duration-300 shadow-lg">
+              <div className="mb-2">
+                <h2 className="font-semibold text-2xl">Description</h2>
+                <p className="">{place.description}</p>
               </div>
-              <div className="grid">
-                {place.photos?.[3] && (
-                  <img
-                    className="aspect-square object-cover"
-                    src={'http://localhost:4000/uploads/' + place.photos[3]}
-                    alt={place.photos[3]}
-                  />
-                )}
-                <div className="overflow-hidden">
-                  {place.photos?.[4] && (
-                    <img
-                      className="aspect-square object-cover relative top-2"
-                      src={'http://localhost:4000/uploads/' + place.photos[4]}
-                      alt={place.photos[4]}
-                    />
-                  )}
-                </div>
-              </div>
+              <h2 className="font-semibold">
+                Check In: <span className="font-normal">{place.checkIn}</span>
+              </h2>
+              <br />
+              <h2 className="font-semibold">
+                Check Out: <span className="font-normal">{place.checkOut}</span>
+              </h2>
+              <br />
+              <h2 className="font-semibold">
+                Max Number of Guest:{' '}
+                <span className="font-normal">{place.maxGuests}</span>
+              </h2>
             </div>
-            <button
-              onClick={() => {
-                setShowAllPhotos(true);
-              }}
-              className="absolute bottom-2 right-2 py-2 px-3 bg-zinc-300 rounded-2xl shadow-md shadow-slate-800 flex gap-1 items-center duration-300 hover:scale-110 hover:bg-rose-500">
-              <TbGridDots />
-              Show more Photos
-            </button>
+            <div className="">
+              <Booking placeProps={place} />
+            </div>
           </div>
         </div>
       )}
